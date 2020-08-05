3f247691558bdc868e9eac60f9803b4c7cab3667
rahul4ror <rahul4ror@gmail.com>
2020-08-05 21:34:41 +0530

    Initializes the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsApiApp::Application.config.secret_key_base = 'd34d665a8cb046e36243a6c9cd061ab1b7cd3abd65c6971a5d155eb6238962937669568b86aed0b60aa33ab362e3261f066d4e8050e964e4052d2615cdce462c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsApiApp::Application.config.session_store :cookie_store, key: '_rails_api_app_session'
